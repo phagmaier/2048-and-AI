2b29c79abb4064b29285a7ee634920c03a3d25a5
phagmaier <parkerhagmaier@gmail.com>
2024-08-04 18:30:05 -0700

    very stupid problem I found in monte carlo working on fixing added debugging prints

---
+++ bin/CMakeFiles/runme.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runme.dir/brain.cpp.o"
+  "CMakeFiles/runme.dir/brain.cpp.o.d"
+  "CMakeFiles/runme.dir/main.cpp.o"
+  "CMakeFiles/runme.dir/main.cpp.o.d"
+  "runme"
+  "runme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
